46552847fb55d36aee9a7217f173624a2a280446
Megan Fuhr <MeganFuhr@gmail.com>
2021-10-12 16:18:40 -0400

    maybe work

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,6 +1,6 @@
 //import { fetch } from 'node-fetch'
-//import fetch from "node-fetch"
-const fetch = require('node-fetch')
+import * as fetch from "node-fetch"
+//const fetch = require('node-fetch')
 
 exports.handler = async function () {
     
