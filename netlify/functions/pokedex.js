68bda4b66a38dd668e6f97a88ab1bb0cbaf19795
Megan Fuhr <MeganFuhr@gmail.com>
2021-10-12 15:52:01 -0400

    :rocket: worked
    worked - needed 'import fetch from "node-fetch"'

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,16 @@
+//import { fetch } from 'node-fetch'
+import fetch from "node-fetch"
+//const fetch = require('node-fetch')
+
+exports.handler = async function () {
+    
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+
+    const response = await fetch(POKE_API)
+    const data = await response.json()
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }
+}
